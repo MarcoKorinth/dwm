291041e0465d3334b06fb24380809ad7e4866a18
Marco Korinth <marco@bizfactory.tech>
2024-06-12 23:09:22 +0200

    update quickinstall

--- quickinstall.sh
+++ quickinstall.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
-apt install make gcc libx11-dev libxft-dev libxinerama-dev xorg
+sudo apt install make gcc libx11-dev libxft-dev libxinerama-dev xorg
 
 echo "Installing dwm..."
 dwmVersion="dwm-6.5.tar.gz"
 wget https://dl.suckless.org/dwm/${dwmVersion}
-tar -xfc ${dwmVersion}
+tar -xzf ${dwmVersion}
 
 echo "Generating xinitrc..."
-echo "exec dwm" >$HOME/.xinitrc
+echo "xrdb -merge ~/.Xresources" >$HOME/.xinitrc
+echo "exec dwm" >>$HOME/.xinitrc
